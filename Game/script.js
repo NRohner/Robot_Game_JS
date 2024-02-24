56115eab3810b9c179810f12d234be3901c98601
BigBeast420 <62449619+BigBeast420@users.noreply.github.com>
2024-02-24 10:10:48 +0100

    Starting to establish structure for re-built game

---
+++ Game/script.js
@@ -0,0 +1,42 @@
+// All code for the robot game will go here.
+
+// Global Constants:
+
+
+
+// Adjustable Input Variables:
+
+
+
+// Global Functions
+    // findArcIntersect
+
+
+
+// Game class - Overarching class that runs the entire game
+
+
+
+
+// Robot Class
+
+
+
+
+
+// Primary Arm Class
+
+
+
+
+// Secondary Arm Class
+
+
+
+
+// Payload Class
+
+
+
+
+// Dropoff Class
