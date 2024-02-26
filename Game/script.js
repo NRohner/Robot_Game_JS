b29bed7ad41bbc63390086de3dc3091408a6491a
BigBeast420 <62449619+BigBeast420@users.noreply.github.com>
2024-02-26 15:24:13 +0100

    Showing ppl at work

--- Game/script.js
+++ Game/script.js
@@ -13,8 +13,8 @@ const secondaryArmI = 1;
 
 // Physics Constants and Variables:
 const g = 9.81;    // Acceleration due to gravity
-const deltaT = 0.003        
-const frameTime = 1000/60    //Time step for 60 calculations per second. Will need to pair this with the animation function for it to work right
+const deltaT = 0.003;        
+const frameTime = 1000/60;    //Time step for 60 calculations per second. Will need to pair this with the animation function for it to work right
 
 // Canvas Constants:
 
@@ -480,8 +480,7 @@ window.addEventListener("keyup", function (event) {
 
 
 
-// Testing
-
+// Actual running of the game
 window.onload = function(){     //JS will wait for the entier page to load including all images and external content before trigging the window.onload
     canvas = document.getElementById('Canvas1');  //Creating a constant called canvas and setting it equal to the canvas1 canvas we created back in the HTML file
     ctx = canvas.getContext('2d');
